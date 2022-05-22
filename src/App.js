b83ace6098d04304b4166b9eec4ec51c824a0931
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-22 16:50:39 -0300

    add link to navigation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetups from './pages/AllMeetups';
 import NewMeetups from './pages/NewMeetup';
 import FavoriteMeetups from './pages/Favorites';
-
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path='/'>
           <AllMeetups />
