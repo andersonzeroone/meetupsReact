3436ae97e22a260df1b12bee8430a5bc52895de4
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-22 16:22:54 -0300

    config routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Route, Switch } from 'react-router-dom'
+
+
+import AllMeetups from './pages/AllMeetups';
+import NewMeetups from './pages/NewMeetup';
+import FavoriteMeetups from './pages/Favorites';
+
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path='/'>
+          <AllMeetups />
+        </Route>
+
+
+        <Route path='/new-meetup'>
+          < NewMeetups />
+        </Route>
+
+
+        <Route path='/favorites'>
+          <FavoriteMeetups />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
