b9643fd2c86ef5e169a4882c9008be28d967b0fd
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-23 13:48:08 -0300

    create component Layout

--- src/App.js
+++ src/App.js
@@ -4,28 +4,28 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetups from './pages/AllMeetups';
 import NewMeetups from './pages/NewMeetup';
 import FavoriteMeetups from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+
+import Layout from './components/layout/Layout'
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+
+    <Layout>
+
       <Switch>
         <Route path='/'>
           <AllMeetups />
         </Route>
 
-
         <Route path='/new-meetup'>
           < NewMeetups />
         </Route>
-
 
         <Route path='/favorites'>
           <FavoriteMeetups />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   )
 }
 
